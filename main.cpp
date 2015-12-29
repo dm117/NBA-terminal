07f6404573026dfa34ed3dd89a6201936da5f0bd
daniel maria <dmb.ball@gmail.com>
2015-12-29 05:12:11 -0800

    updated readme, changed all inputs to vectors

--- main.cpp
+++ main.cpp
@@ -22,21 +22,23 @@
 #include <fstream>
 #include <ctime>
 #include <sstream> //for stringstream: convert int to string
+#include <vector>
 
 using namespace std;
 
 void weekConvert(int &tempWeek_, string &pcWeek_); //converts tempWeek
 void monthConvert(int &tempMonth_, string &pcMonth_); //converts tempMonth
-void dataInput(ifstream &schedule_, string &weekDay_, string &time_, string &box_, string &team1_, string &team1Score_, string &team2_, string &team2Score_, string &OT_, string &notes_); //gets data from file
-
+void dataInput(ifstream &schedule_, vector<string> &weekDay_, vector<string> &time_, vector<string> &box_, vector<string> &team1_, vector<string> &team1Score_, vector<string> &team2_,
+vector<string> &team2Score_, vector<string> &OT_, vector<string> &notes_, string userString); //gets data from file
 int main()
 {
     int changeDir, newDir;
     ifstream schedule;
-    string a, b, c, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes;
+    string a, b, c, userString; //to ignore first three lines
+    vector<string> weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes;
     string pcWeek, pcMonth, pcDay, pcYear; //system dates
     string pcDate; //final string for system date
-    int tempMonth, tempYear, tempDay, tempWeek;
+    int tempMonth, tempYear, tempDay, tempWeek, i = 0;
     stringstream convertYear, convertDay;
 
     time_t t = time(0);   // get time now
@@ -72,131 +74,151 @@ int main()
         getline(schedule, b);
         getline(schedule, c);
 
-        dataInput(schedule, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes); //first read of the file
+        dataInput(schedule, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes, userString); //first read of the file
 
         do
         {
-            if (weekDay != pcDate)
+            if (weekDay[i] != pcDate)
             {
-                dataInput(schedule, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes); //calling the function if system date doesnt match
+                dataInput(schedule, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes, userString); //calling the function if system date doesnt match
             }
             else
             {
-                cout << weekDay << endl;
-                cout << times << endl;
-                cout << team1 << " " << team1Score << endl;
-                cout << team2 << " " << team2Score << endl;
-                cout << OT << endl;
-                cout << notes << endl;
+                cout << weekDay[i] << endl;
+                cout << times[i] << endl;
+                cout << team1[i] << " " << team1Score[i] << endl;
+                cout << team2[i] << " " << team2Score[i] << endl;
+                cout << OT[i] << endl;
+                cout << notes[i] << endl;
                 cout << "\n" << endl;
 
-                dataInput(schedule, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes); //calling the input function
+                dataInput(schedule, weekDay, times, box, team1, team1Score, team2, team2Score, OT, notes, userString); //calling the input function
 
             }
+            i++;
 
-        }
-
-        while(!schedule.eof());
+        }while(!schedule.eof());
 
         schedule.close();
     }
     else
     {
-        cout << "Could not open the file" << endl;
+        cout << "Try running the program again, couldn't open the file" << endl;
     }
 
     cout << "\nEnd of program" << endl;
+    cout << pcDate << endl;
 
     return 0;
 }
 
 void weekConvert(int &tempWeek_, string &pcWeek_)
 {
-switch ( tempWeek_ ) {
-  case 0:
-    pcWeek_ = "Sun";
-    break;
-  case 1:
-    pcWeek_ = "Mon";
-    break;
-  case 2:
-    pcWeek_ = "Tue";
-    break;
-  case 3:
-    pcWeek_ = "Wed";
-    break;
-  case 4:
-    pcWeek_ = "Thu";
-    break;
-  case 5:
-    pcWeek_ = "Fri";
-    break;
-  case 6:
-    pcWeek_ = "Sat";
-    break;
-  default:
-    cout<<"ERROR GETTING DAY OF THE WEEK\n";
-    break;
-  }
+    switch ( tempWeek_ )
+    {
+    case 0:
+        pcWeek_ = "Sun";
+        break;
+    case 1:
+        pcWeek_ = "Mon";
+        break;
+    case 2:
+        pcWeek_ = "Tue";
+        break;
+    case 3:
+        pcWeek_ = "Wed";
+        break;
+    case 4:
+        pcWeek_ = "Thu";
+        break;
+    case 5:
+        pcWeek_ = "Fri";
+        break;
+    case 6:
+        pcWeek_ = "Sat";
+        break;
+    default:
+        cout<<"ERROR GETTING DAY OF THE WEEK\n";
+        break;
+    }
 }
 
 void monthConvert(int &tempMonth_, string &pcMonth_)
 {
-switch ( tempMonth_ ) {
-  case 1:
-    pcMonth_ = "Jan";
-    break;
-  case 2:
-    pcMonth_ = "Feb";
-    break;
-  case 3:
-    pcMonth_ = "March";
-    break;
-  case 4:
-    pcMonth_ = "Apr";
-    break;
-  case 5:
-    pcMonth_ = "May";
-    break;
-  case 6:
-    pcMonth_ = "Jun";
-    break;
-  case 7:
-    pcMonth_ = "Jul";
-    break;
-  case 8:
-    pcMonth_ = "Aug";
-    break;
-  case 9:
-    pcMonth_ = "Sep";
-    break;
-  case 10:
-    pcMonth_ = "Oct";
-    break;
-  case 11:
-    pcMonth_ = "Nov";
-    break;
-  case 12:
-    pcMonth_ = "Dec";
-    break;
-  default:
-    cout<<"ERROR\n";
-    break;
-  }
+    switch ( tempMonth_ )
+    {
+    case 1:
+        pcMonth_ = "Jan";
+        break;
+    case 2:
+        pcMonth_ = "Feb";
+        break;
+    case 3:
+        pcMonth_ = "March";
+        break;
+    case 4:
+        pcMonth_ = "Apr";
+        break;
+    case 5:
+        pcMonth_ = "May";
+        break;
+    case 6:
+        pcMonth_ = "Jun";
+        break;
+    case 7:
+        pcMonth_ = "Jul";
+        break;
+    case 8:
+        pcMonth_ = "Aug";
+        break;
+    case 9:
+        pcMonth_ = "Sep";
+        break;
+    case 10:
+        pcMonth_ = "Oct";
+        break;
+    case 11:
+        pcMonth_ = "Nov";
+        break;
+    case 12:
+        pcMonth_ = "Dec";
+        break;
+    default:
+        cout<<"ERROR\n";
+        break;
+    }
 }
 
-void dataInput(ifstream &schedule_, string &weekDay_, string &time_, string &box_, string &team1_, string &team1Score_, string &team2_, string &team2Score_, string &OT_, string &notes_) //gets data from file
+void dataInput(ifstream &schedule_, vector<string> &weekDay_, vector<string> &time_, vector<string> &box_, vector<string> &team1_, vector<string> &team1Score_, vector<string> &team2_,
+vector<string> &team2Score_, vector<string> &OT_, vector<string> &notes_, string userString_) //gets data from file
 {
 
-    getline(schedule_, weekDay_, ',');
-    getline(schedule_, time_, ',');
-    getline(schedule_, box_, ',');
-    getline(schedule_, team1_, ',');
-    getline(schedule_, team1Score_, ',');
-    getline(schedule_, team2_, ',');
-    getline(schedule_, team2Score_, ',');
-    getline(schedule_, OT_, ',');
-    getline(schedule_, notes_);
+    getline(schedule_, userString_, ',');
+    weekDay_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    time_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    box_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    team1_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    team1Score_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    team2_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    team2Score_.push_back(userString_);
+
+    getline(schedule_, userString_, ',');
+    OT_.push_back(userString_);
+
+    getline(schedule_, userString_);
+    notes_.push_back(userString_);
 
 }
 
